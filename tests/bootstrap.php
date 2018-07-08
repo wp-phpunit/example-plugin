0b1c0efcb963ce9d59f83613d351f205e1fc1f37
Evan Mattson <me@aaemnnost.tv>
2018-07-08 19:20:43 +0200

    always use WP_PHPUNIT__DIR in bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,7 +7,7 @@
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 // Give access to tests_add_filter() function.
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
     // test set up, plugin activation, etc.
@@ -15,4 +15,4 @@ tests_add_filter( 'muplugins_loaded', function() {
 } );
 
 // Start up the WP testing environment.
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
+require getenv( 'WP_PHPUNIT__DIR' ) . '/includes/bootstrap.php';
