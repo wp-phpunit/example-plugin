339f9a6f00042d4f4e8d468aecaa36a14e97f1e4
Evan Mattson <me@aaemnnost.tv>
2018-07-08 19:11:43 +0200

    update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,17 +6,8 @@
 // Composer autoloader must be loaded before WP_PHPUNIT__DIR will be available
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
-// Determine the test suite to use.
-$wp_version = getenv( 'WP_VERSION' );
-$tests_dir = getenv( 'WP_TESTS_DIR' );
-
-// Use WP-PHPUnit if no WP_VERSION is set or explicitly calls for Composer-installed version
-if ( ! $wp_version || 'composer' === $wp_version ) {
-    $tests_dir = getenv( 'WP_PHPUNIT__DIR' );
-}
-
 // Give access to tests_add_filter() function.
-require_once $tests_dir . '/includes/functions.php';
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
     // test set up, plugin activation, etc.
@@ -24,5 +15,4 @@ tests_add_filter( 'muplugins_loaded', function() {
 } );
 
 // Start up the WP testing environment.
-require $tests_dir . '/includes/bootstrap.php';
-
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
